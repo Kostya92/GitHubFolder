d473ccb1c3a896917626e142385360c832ee61ab
Kostya <gromovkostya92@fmail.com>
2015-03-09 21:35:42 +0200

    EF Data Base

---
+++ DBFirstExample/DBFirstExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBFirstExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DBFirstDataBaseEntities context = new DBFirstDataBaseEntities();
+
+            var query = from Customers in context.Customers select Customers;
+
+            foreach (var customer in query)
+            {
+                Console.WriteLine(customer.Name);
+                Console.WriteLine(customer.Telephone_Number);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
